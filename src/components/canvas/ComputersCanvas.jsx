8a686b616315c2b0b414a00de502dfbff31e5b44
safwankajas <safwankajas02@gmail.com>
2023-04-07 00:39:19 +0530

    about

--- src/components/canvas/ComputersCanvas.jsx
+++ src/components/canvas/ComputersCanvas.jsx
@@ -1,12 +1,12 @@
 import React, { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import { OrbitControls, Preload, useAnimations, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "../Loader";
 
+
 const Computers = ({ isMobile }) => {
   const computer = useGLTF("./planet/watch.glb");
-
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor='black' />
