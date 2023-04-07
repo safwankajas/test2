b2e403c4b0c99c336508ab1823bba1035112c956
safwankajas <safwankajas02@gmail.com>
2023-04-07 07:50:50 +0530

    about_mobile_clr_er_again_clr

--- src/style.js
+++ src/style.js
@@ -11,7 +11,7 @@ const styles = {
     sectionHeadText:
       "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
     sectionSubText:
-      "sm:text-[18px] text-[14px] bg-clip-text text-transparent   bg-gradient-to-r from-green-400 to-blue-500 uppercase tracking-wider",
+      "sm:text-[18px] text-[14px] bg-clip-text text-[#915eff] uppercase tracking-wider",
   };
   
   export { styles };
