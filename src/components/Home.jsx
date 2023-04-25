e883429b3a0bae302932ffa985e679672e460dc1
safwankajas <84768183+safwankajas@users.noreply.github.com>
2023-04-25 13:27:41 +0530

    h

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -22,7 +22,7 @@ const Home = () => {
       
       </div>
     <ComputersCanvas />
-      <div className='absolute xs:bottom-0 xs:h-64 bg-white bottom-32 w-full flex justify-center items-center'>
+      <div className='absolute xs:bottom-10 xs:h-auto  h-64 bg-white bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
