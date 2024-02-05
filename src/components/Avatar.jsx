8af252a04aa731d21d4c4a6cbc04df86dfc4f406
safwankajas <safwankajas02@gmail.com>
2024-02-05 12:40:48 +0530

    home -animayion

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -7,7 +7,7 @@ import { useAnimations, useFBX, useGLTF } from "@react-three/drei";
 
 function Avatar(props) {
   const group=useRef();
-  const { nodes, materials } = useGLTF("models/c.glb");
+  const { nodes, materials } = useGLTF("models/d.glb");
   const {animations:wathcAnimation}=useFBX("animations/watchfbx.fbx")
   wathcAnimation[0].name="watch";
   const { actions}=useAnimations(wathcAnimation,group);
@@ -36,4 +36,4 @@ function Avatar(props) {
   );
 }export default Avatar;
 
-useGLTF.preload("models/c.glb");
+useGLTF.preload("models/d.glb");
